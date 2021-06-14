db5345607d11193a6c4e956a625b5e15083594ce
Annu Rathore <46515932+komalrathore786@users.noreply.github.com>
2021-06-15 01:12:45 +0530

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,39 @@
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "KomalRathore"
+        workspaces {
+            prefix = "gh-actions-demo"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "aws_static_site" {
+    source  = "dvargas92495/static-site/aws"
+    version = "1.2.0"
+
+    secret = "secret-key"
+    tags = {
+        Application = "static-site-demo"
+    }
+}
+
+provider "github" {
+    owner = "komalrathore786"
+}
+
+resource "github_actions_secret" "deploy_aws_access_key" {
+  repository       = "ReactApp"
+  secret_name      = "DEPLOY_AWS_ACCESS_KEY_ID"
+  plaintext_value  = module.aws_static_site.deploy-id     
+}
+
+resource "github_actions_secret" "deploy_aws_access_secret" {
+  repository       = "ReactApp"
+  secret_name      = "DEPLOY_AWS_SECRET_ACCESS_KEY"       
+  plaintext_value  = module.aws_static_site.deploy-secret 
+}
